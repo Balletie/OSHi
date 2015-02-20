3872b7cf9de332f179730bd87fc9da4f45c4d1c4
Balletie <skip_meesie@hotmail.com>
2015-02-20 16:52:03 +0100

    Make subst a static function for encapsulation, add comment for macros

--- src/util.h
+++ src/util.h
@@ -4,6 +4,8 @@
 #define RED_B "\033[1m\033[31m"
 #define RESET "\033[0m"
 
+/** Some utility macros for error reporting. Shows a nice fat bold red "error"
+ * text.*/
 #define osh_str_error(s) fprintf(stderr, RED_B"error: "RESET"%s\n", s)
 #define osh_error(x) osh_str_error(strerror(errno))
 
